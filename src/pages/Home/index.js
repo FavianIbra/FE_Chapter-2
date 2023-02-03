a5380bd46fe20fa4a58d4a75a96ca86d657ac2ce
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-02-03 14:57:26 +0700

    Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import '../../css/style.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <section className="home" id='home'>
+        <div className="max-width">
+          <div className="home-content">
+            <div className="judul-1">
+              <span>Welcome to</span>
+              <div className="typing">
+                <span>kelompok 2</span>
+              </div>
+            </div>
+            <div className="isi">
+              <p>Sekelompok orang yang suka dengan UKL dan dia</p>
+              <a href="#about">
+                <span></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Home;
